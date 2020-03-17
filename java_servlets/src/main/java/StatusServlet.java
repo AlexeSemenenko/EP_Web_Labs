b1b5c1e32c406364593ab9aafd1e459595b295c5
AlexSemenenko <aleksei.syemyenenko@gmail.com>
2020-03-17 11:59:35 +0300

    fixed html insert

--- java_servlets/src/main/java/StatusServlet.java
+++ java_servlets/src/main/java/StatusServlet.java
@@ -9,6 +9,6 @@ public class StatusServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter pw = response.getWriter();
 
-        pw.println("<html><h2 style='color:red'>Application is running</h2></html>");
+        pw.println("<big style='color:red'>Application is running</big>");
     }
 }
